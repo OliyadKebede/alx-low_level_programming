844cf2685c3347a427ab4fbf036a0ed8171ccf91
Oliyad Kebede <81267448+OliyadKebede@users.noreply.github.com>
2022-03-11 13:17:20 -0800

    add : file

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,12 +8,12 @@ int main(void)
 {
 int z;
 
-		for(z = 48; z <= 57; z++)
+    for(z = 48; z <= 57; z++)
     putchar(z);
 
     for (z = 97; z <= 102;z++)
     putchar(z);
 
     putchar('\n');
-	return (0);
+return (0);
 }
