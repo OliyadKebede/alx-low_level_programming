d14386f79259866a58bf7d81b68561384d54a0fc
Oliyad Kebede <81267448+OliyadKebede@users.noreply.github.com>
2022-04-03 00:03:32 +0300

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,55 +1,85 @@
-#​include​ ​<​stdio.h​> 
-​#​include​ ​<​stdlib.h​> 
-#​include​ ​<​string.h​> 
-​#​include​ ​<​ctype.h​> 
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
 
-​/*​* 
- ​ * main - prints the minimum number of coins 
- ​ * to make change for an amount of money 
- ​ * @argc: n args 
- ​ * @argv: arr args 
- ​ * Return: 0 
- ​ ​*/ 
-​int​ ​main​(​int​ argc, ​char​ *argv[]) 
-{ 
- ​       ​int​ value, c; 
-  
- ​       c = ​0​; 
- ​       ​if​ (argc != ​2​) 
- ​       { 
- ​               ​printf​(​"​Error​\n​"​); 
- ​               ​return​ (​1​); 
- ​        } 
- ​       value = ​atoi​(argv[​1​]); 
- ​       ​if​ (value < ​0​) 
- ​        { 
- ​                ​printf​(​"​%d​\n​"​, ​0​); 
- ​                ​return​ (​0​); 
- ​        } 
- ​        ​if​ (value % ​25​ >= ​0​) 
- ​        { 
- ​                c += value / ​25​; 
- ​                value = value % ​25​; 
- ​        } 
- ​        ​if​ (value % ​10​ >= ​0​) 
- ​        { 
- ​                c += value / ​10​; 
- ​                value = value % ​10​; 
- ​        } 
- ​        ​if​ (value % ​5​ >= ​0​) 
- ​        { 
- ​                c += value / ​5​; 
- ​                value = value % ​5​; 
- ​        } 
- ​        ​if​ (value % ​2​ >= ​0​) 
- ​        { 
- ​                c += value / ​2​; 
- ​                value = value % ​2​; 
- ​        } 
- ​        ​if​ (value % ​1​ >= ​0​) 
- ​        { 
- ​                c += value / ​1​; 
- ​        } 
- ​        ​printf​(​"​%d​\n​"​, c); 
- ​        ​return​ (​0​); 
- ​}
+
+
+
+
+
+/**
+ * coin_count - counts the min coins needed for change
+ *
+ * @argv: argument vector
+ * @count: int to count coins
+ * Return: int
+ */
+int coin_count(char *argv[], int count)
+{
+int sum = 0;
+
+while (!(sum + 25 > atoi(argv[1])))
+{
+sum += 25;
+count++;
+}
+while (!(sum + 10 > atoi(argv[1])))
+{
+sum += 10;
+count++;
+}
+while (!(sum + 5 > atoi(argv[1])))
+{
+sum += 5;
+count++;
+}
+while (!(sum + 2 > atoi(argv[1])))
+{
+sum += 2;
+count++;
+}
+while (!(sum + 1 > atoi(argv[1])))
+{
+sum += 1;
+count++;
+}
+return (count);
+}
+
+
+
+
+
+
+
+
+/**
+ * main - prints the min num of coins to make change for an amount of money
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: int
+ */
+
+
+
+
+
+int main(int argc, char *argv[])
+{
+int count = 0;
+
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+else if (atoi(argv[1]) < 0)
+{
+printf("%d\n", 0);
+return (0);
+}
+count = coin_count(argv, count);
+printf("%d\n", count);
+return (0);
+}
