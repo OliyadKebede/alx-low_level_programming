185344f81ad1a40a42d7f4787eb029c44b352109
Oliyad Kebede <81267448+OliyadKebede@users.noreply.github.com>
2022-04-02 23:58:17 +0300

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,38 +1,56 @@
- #​include​ ​<​stdio.h​> 
+  
+ ​#​include​ ​<​stdio.h​> 
  ​#​include​ ​<​stdlib.h​> 
+ ​#​include​ ​<​string.h​> 
+ ​#​include​ ​<​ctype.h​> 
   
  ​/*​* 
- ​ * main - function to print out the change that needs to given 
- ​ * @argc: number of arguments to be passed 
- ​ * @argv: number of variables to calculate 
- ​ * Return: always 0 for sucess 
+ ​ * main - prints the minimum number of coins 
+ ​ * to make change for an amount of money 
+ ​ * @argc: n args 
+ ​ * @argv: arr args 
+ ​ * Return: 0 
  ​ ​*/ 
-  
- ​int​ ​main​(​int​ argc, ​char​ **argv) 
+ ​int​ ​main​(​int​ argc, ​char​ *argv[]) 
  ​{ 
- ​        ​int​ total, change; 
+ ​        ​int​ value, c; 
   
- ​        ​if​ (argc < ​2​) 
+ ​        c = ​0​; 
+ ​        ​if​ (argc != ​2​) 
  ​        { 
  ​                ​printf​(​"​Error​\n​"​); 
  ​                ​return​ (​1​); 
  ​        } 
-  
- ​        change = ​atoi​(argv[​1​]); 
-  
- ​        ​for​ (total = ​0​; change > ​0​; total++) 
+ ​        value = ​atoi​(argv[​1​]); 
+ ​        ​if​ (value < ​0​) 
  ​        { 
- ​                ​if​ (change - ​25​ >= ​0​) 
- ​                        change = change - ​25​; 
- ​                ​else​ ​if​ (change - ​10​ >= ​0​) 
- ​                        change = change - ​10​; 
- ​                ​else​ ​if​ (change - ​5​ >= ​0​) 
- ​                        change = change - ​5​; 
- ​                ​else​ ​if​ (change - ​2​ >= ​0​) 
- ​                        change = change - ​2​; 
- ​                ​else​ ​if​ (change - ​1​ >= ​0​) 
- ​                        change = change - ​1​; 
+ ​                ​printf​(​"​%d​\n​"​, ​0​); 
+ ​                ​return​ (​0​); 
  ​        } 
- ​        ​printf​(​"​%d​\n​"​, total); 
+ ​        ​if​ (value % ​25​ >= ​0​) 
+ ​        { 
+ ​                c += value / ​25​; 
+ ​                value = value % ​25​; 
+ ​        } 
+ ​        ​if​ (value % ​10​ >= ​0​) 
+ ​        { 
+ ​                c += value / ​10​; 
+ ​                value = value % ​10​; 
+ ​        } 
+ ​        ​if​ (value % ​5​ >= ​0​) 
+ ​        { 
+ ​                c += value / ​5​; 
+ ​                value = value % ​5​; 
+ ​        } 
+ ​        ​if​ (value % ​2​ >= ​0​) 
+ ​        { 
+ ​                c += value / ​2​; 
+ ​                value = value % ​2​; 
+ ​        } 
+ ​        ​if​ (value % ​1​ >= ​0​) 
+ ​        { 
+ ​                c += value / ​1​; 
+ ​        } 
+ ​        ​printf​(​"​%d​\n​"​, c); 
  ​        ​return​ (​0​); 
  ​}
