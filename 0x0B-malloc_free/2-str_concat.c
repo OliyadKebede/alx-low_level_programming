0bdaeecdf085decec708e0fa408bff0ab06e5eb9
Oliyad Kebede <81267448+OliyadKebede@users.noreply.github.com>
2022-04-05 11:37:37 -0700

    memory management

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,33 +1,52 @@
-#include "holberton.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * _strlen - find length of a string
+ * @s: string
+ * Return: int
+ */
+
+
+int _strlen(char *s)
+{
+int size = 0;
+for (; s[size] != '\0'; size++)
+;
+return (size);
+}
+
+/**
+ * *str_concat - concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: pointer
+ */
 
 char *str_concat(char *s1, char *s2)
 {
-  int i, j, k;
-  char *newStr = NULL;
+int size1, size2, i;
+char *m;
 
-  if (s1 == NULL)
-    s1 = "";
-  if (s2 == NULL)
-    s2 = "";
+if (s1 == NULL)
+	s1 = "\0";
+if (s2 == NULL)
+	s2 = "\0";
 
-  for (i = 0; s1[i] != '\0'; i++)
-    ;
+size1 = _strlen(s1);
+size2 = _strlen(s2);
+m = malloc((size1 + size2) *sizeof(char) + 1);
+if (m == 0)
+	return (0);
 
-  for (j = 0; s2[j] != '\0'; j++)
-    ;
-
-  newStr = (char*)malloc((i + j) * sizeof(char));
-
-  if (newStr == NULL)
-    return (NULL);
-
-  for (k = 0; s1[k] != '\0'; k++)
-    newStr[k] = s1[k];
-
-  for (k = 0; s2[k] != '\0'; k++)
-    newStr[k + i] = s2[k];
-
-  return (newStr);
+for (i = 0; i <= size1 + size2; i++)
+{
+	if (i < size1)
+		m[i] = s1[i];
+	else
+		m[i] = s2[i - size1];
+}
+m[i] = '\0';
+return (m);
 }
