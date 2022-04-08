83385161ec6942eed7ed7303c815c014b529b8b8
Oliyad Kebede <81267448+OliyadKebede@users.noreply.github.com>
2022-04-08 13:50:24 -0700

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,213 +1,147 @@
 #include "holberton.h"
-#include <stdio.h>
 #include <stdlib.h>
-/**
- * add - addition of parcial products
- * @p: parcial product
- * @mg: big malloc
- * @i: size of big malloc - 1
- * @tp: size of parcial product - 1
-  */
-void add(char *p, char *mg, int i, int tp)
-{
-	int pos = tp, a = 0, b = 0, car = 0, res = 0;
-	int cont = tp, aux = 0;
 
-	for (; cont >=  0; cont--, i--, pos--)
+/**
+ * _print - moves a string one place to the left and prints the string
+ * @str: string to move
+ * @l: size of string
+ *
+ * Return: void
+ */
+void _print(char *str, int l)
+{
+	int i, j;
+
+	i = j = 0;
+	while (i < l)
 	{
-		a = *(p + pos) - '0';
-		b = *(mg + i) - '0';
-		res = (a + b) + car;
-		car = res / 10;
-		res = (res % 10) + '0';
-		*(mg + i) = res;
-	}
-	res = 0;
-	for (aux = i - 1; aux >= 0; aux--)
-	{
-		a = *(mg + aux) - '0';
-		a += a + car;
-		car = a / 10;
-		a = (a % 10) + '0';
-		*(mg + aux) = a;
-	}
-}
-/**
- * setiar - fill the pointer with 0
- * @s: pointer
- * @a: size of pointer
- */
-void setiar(char *s, int a)
-{
-	int i;
-
-	for (i = 0; i < a; i++)
-		s[i] = '0';
-}
-/**
- * parcial - give the parcial malloc
- * @a: size one
- * Return: parcial malloc
- */
-char *parcial(int a)
-{
-	int i;
-	char *p;
-
-	p = malloc(a);
-	if (!p)
-		return (NULL);
-	for (i = 0; i < a; i++)
-		p[i] = '0';
-	return (p);
-}
-/**
- * grande - give big malloc
- * @a: size one
- * @b: size two
- * Return: malloc
- */
-char *grande(int a, int b)
-{
-	int i;
-	char *p;
-
-	p = malloc(a + b);
-	if (!p)
-		return (NULL);
-	for (i = 0; i < a + b; i++)
-		p[i] = '0';
-	return (p);
-}
-/**
- * tama - give the size
- * @s: string of numbers
- * Return: size
- */
-int tama(char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
+		if (str[i] != '0')
+			j = 1;
+		if (j || i == l - 1)
+			_putchar(str[i]);
 		i++;
 	}
-	return (i);
-}
-/**
- * Perror - print Error with putchar.
- * Return: Always 0.
- */
-void Perror(void)
-{
-	_putchar('E');
-	_putchar('r');
-	_putchar('r');
-	_putchar('o');
-	_putchar('r');
+
 	_putchar('\n');
-	exit(98);
+	free(str);
 }
-/**
- * numero - check if strings have only numbers.
- * @s: string 1
- * @a: string 2
- * Return: Always 1 if not or 0 if only has number.
- */
-void numero(char *s, char *a)
-{
-	int i = 0, aux = 0;
 
-	while (s[i] != '\0')
+/**
+ * mul - multiplies a char with a string and places the answer into dest
+ * @n: char to multiply
+ * @num: string to multiply
+ * @num_index: last non NULL index of num
+ * @dest: destination of multiplication
+ * @dest_index: highest index to start addition
+ *
+ * Return: pointer to dest, or NULL on failure
+ */
+char *mul(char n, char *num, int num_index, char *dest, int dest_index)
+{
+	int j, k, mul, mulrem, add, addrem;
+
+	mulrem = addrem = 0;
+	for (j = num_index, k = dest_index; j >= 0; j--, k--)
 	{
-		if (!(s[i] >= '0' && s[i] <= '9'))
-		{
-			aux = 1;
-			break;
-		}
-		i++;
+		mul = (n - '0') * (num[j] - '0') + mulrem;
+		mulrem = mul / 10;
+		add = (dest[k] - '0') + (mul % 10) + addrem;
+		addrem = add / 10;
+		dest[k] = add % 10 + '0';
 	}
-	i = 0;
-	while (a[i] != '\0')
+	for (addrem += mulrem; k >= 0 && addrem; k--)
 	{
-		if (!(a[i] >= '0' && a[i] <= '9'))
-		{
-			aux = 1;
-			break;
-		}
-		i++;
+		add = (dest[k] - '0') + addrem;
+		addrem = add / 10;
+		dest[k] = add % 10 + '0';
 	}
-	if (aux == 1)
-		Perror();
-	if (*s == '0' || *a == '0')
+	if (addrem)
 	{
-		_putchar('0');
-		_putchar('\n');
-		exit(0);
+		return (NULL);
 	}
+	return (dest);
 }
 /**
- * impresion - print the pointer
- * @s: string.
- * @a: size of string
+ * check_for_digits - checks the arguments to ensure they are digits
+ * @av: pointer to arguments
+ *
+ * Return: 0 if digits, 1 if not
  */
-void impresion(char *s, int a)
+int check_for_digits(char **av)
+{
+	int i, j;
+
+	for (i = 1; i < 3; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			if (av[i][j] < '0' || av[i][j] > '9')
+				return (1);
+		}
+	}
+	return (0);
+}
+
+/**
+ * init - initializes a string
+ * @str: sting to initialize
+ * @l: length of strinf
+ *
+ * Return: void
+ */
+void init(char *str, int l)
 {
 	int i;
 
-	for (i = 0; i < a; i++)
-	{
-		if (!(s[0] == '0' && i == 0))
-			_putchar(s[i]);
-	}
-	_putchar('\n');
+	for (i = 0; i < l; i++)
+		str[i] = '0';
+	str[i] = '\0';
 }
+
 /**
- * main - check the code for Holberton School students.
- * @argv: array of pointers store the arguments
- * @argc: quantity of arguments
- * Return: Always 0.
+ * main - multiply two numbers
+ * @argc: number of arguments
+ * @argv: argument vector
+ *
+ * Return: zero, or exit status of 98 if failure
  */
 int main(int argc, char *argv[])
 {
-	int t1, t2, t3, a = 0, b = 0, j = 0, aux = 0, car, g, ind;
-	char *mg, *p, *size1, *size2;
+	int l1, l2, ln, ti, i;
+	char *a;
+	char *t;
+	char e[] = "Error\n";
 
-	if (argc != 3)
-		Perror();
-	else
-	{ numero(argv[1], argv[2]);
-		t1 = tama(argv[1]);
-		t2 = tama(argv[2]);
-		ind = t1 + t2;
-		mg = grande(t1, t2);
-		if (t1 >= t2)
-		{ size1 = argv[1];
-			size2 = argv[2];
-			b = t1 - 1;
-			a = t2 - 1;
-			t3 = t1 + 1; }
-		else
-		{ size1 = argv[2];
-			size2 = argv[1];
-			b = t2 - 1;
-			a = t1 - 1;
-			t3 = t2 + 1; }
-		g = b;
-		p = parcial(t3);
-		j = b + 1;
-		for (; a >= 0; a--, ind--)
-		{ car = 0;
-			b = g;
-			j = g + 1;
-			setiar(p, t3);
-			for (; b >= 0; b--, j--)
-			{ aux = ((size2[a] - '0') * (size1[b] - '0'));
-				aux = aux + car;
-				p[j] = ((aux % 10) + '0');
-				car = aux / 10;	}
-			p[j] = (car + '0');
-			add(p, mg, (ind - 1), (t3 - 1)); }
-		impresion(mg, (t1 + t2)); }
+	if (argc != 3 || check_for_digits(argv))
+	{
+		for (ti = 0; e[ti]; ti++)
+			_putchar(e[ti]);
+		exit(98);
+	}
+	for (l1 = 0; argv[1][l1]; l1++)
+		;
+	for (l2 = 0; argv[2][l2]; l2++)
+		;
+	ln = l1 + l2 + 1;
+	a = malloc(ln * sizeof(char));
+	if (a == NULL)
+	{
+		for (ti = 0; e[ti]; ti++)
+			_putchar(e[ti]);
+		exit(98);
+	}
+	init(a, ln - 1);
+	for (ti = l2 - 1, i = 0; ti >= 0; ti--, i++)
+	{
+		t = mul(argv[2][ti], argv[1], l1 - 1, a, (ln - 2) - i);
+		if (t == NULL)
+		{
+			for (ti = 0; e[ti]; ti++)
+				_putchar(e[ti]);
+			free(a);
+			exit(98);
+		}
+	}
+	_print(a, ln - 1);
 	return (0);
 }
