d51b7a08c36b1e9420e33ccbe939d7b9462a98bc
Oliyad Kebede <81267448+OliyadKebede@users.noreply.github.com>
2022-04-26 23:25:41 +0300

    c function reverse lists

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,11 +1,29 @@
+#include <stdlib.h>
 #include "lists.h"
+
 /**
-  * reverse_listint - define function
-  * @head: describe argument
-  * Return: what does it return?
+  * reverse_listint - ...
+  * @head: ...
+  *
+  * Return: ...
   */
 listint_t *reverse_listint(listint_t **head)
 {
-	head++;
-	return (0);
+	listint_t *prev = NULL, *next = NULL;
+
+	if (head)
+	{
+		while (*head)
+		{
+			next = *head;
+			*head = (*head)->next;
+			next->next = prev;
+			prev = next;
+		}
+
+		*head = prev;
+		return (*head);
+	}
+
+	return (NULL);
 }
