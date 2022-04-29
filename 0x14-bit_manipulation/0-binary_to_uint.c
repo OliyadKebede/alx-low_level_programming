aa0159805b0d97ae1d9d1a4f2dcb5c430e2bc80b
Oliyad Kebede <81267448+OliyadKebede@users.noreply.github.com>
2022-04-30 00:29:35 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,30 +1,26 @@
 #include "holberton.h"
 
 /**
- * binary_to_uint - Function that converts binary to decimal
- * @b: Binary passed in as a char array
- * Return: The converted decimal
+ * binary_to_uint - convert binary string to decimal
+ * @b: binary string
+ *
+ * Return: decimal (unsigned int)
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-	int i, mult = 1, count = 0, sum = 0;
+	unsigned int decimal;
+	unsigned int i;
 
-	if (b == NULL)
-		return (0);
-
-	for (i = 0; b[i] != '\0'; i++)
+	for (decimal = 0, i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] == '0' || b[i] == '1')
-			count++;
-		else
+		if (b[i] == '1')
+			decimal = (decimal << 1) | 1;
+		else if (b[i] == '0')
+			decimal <<= 1;
+		else if (b[i] != '0' && b[i] != '1')
 			return (0);
 	}
 
-	for (count -= 1; count >= 0; count--, mult *= 2)
-	{
-		if (b[count] == '1')
-			sum += 1 * mult;
-	}
-
-	return (sum);
+	return (decimal);
 }
