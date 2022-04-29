85c4ef9c9d0332a64278ac0268a5181e7a471201
Oliyad Kebede <81267448+OliyadKebede@users.noreply.github.com>
2022-04-30 00:57:09 +0300

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,26 +1,30 @@
 #include "holberton.h"
-
 /**
- * print_binary - prints the binary representation of a number
- * @n: parameter
+ * print_binary - decimal to binary without use / %
+ * @n: the decimal
+ * Description: convert decimal to binary
+ * section header: the header of this function is holberton.h
+ * Return: no return
  */
 void print_binary(unsigned long int n)
 {
-	int i, count = 0;
-	unsigned long int current;
+	int flag = 0;
+	unsigned long int mask = 1;
 
-	for (i = 63; i >= 0; i--)
+	mask <<= 63;
+	if (n == 0)
+		_putchar('0');
+
+	while (mask > 0)
 	{
-		current = n >> i;
-
-		if (current & 1)
+		if ((n & mask) == 0 && flag == 1)
+			_putchar('0');
+		if ((n & mask) != 0)
 		{
 			_putchar('1');
-			count++;
+			flag = 1;
 		}
-		else if (count)
-			_putchar('0');
+
+		mask = mask >> 1;
 	}
-	if (!count)
-		_putchar('0');
 }
