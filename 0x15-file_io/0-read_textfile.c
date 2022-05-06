d8baafb606cc171b47186fab150f22d23aaf8ce2
Oliyad Kebede <81267448+OliyadKebede@users.noreply.github.com>
2022-05-06 12:33:34 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,41 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "holberton.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include "main.h"
 
 /**
- * read_textfile - that reads a text file and prints
- * @filename: variable pointer
- * @letters: size letters
- * Description: Write a function that reads a text file and prints it
- * to the POSIX standard output.
- * Return: the actual number of letters it could read and print, 0 otherwise
+ * read_textfile - check the code for Holberton School students.
+ * @filename: name of my file
+ * @letters: number of the letters that i used
+ * Return: Always 0.
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t file, let, w;
-	char *text;
+	int ID = 0;
+	ssize_t WR = 0;
+	ssize_t len = 0;
+	char *buff = NULL;
 
-	text = malloc(letters);
-	if (text == NULL)
-		return (0);
-
-	if (filename == NULL)
-		return (0);
-
-	file = open(filename, O_RDONLY);
-
-	if (file == -1)
+	if (filename != NULL)
 	{
-		free(text);
-		return (0);
+		buff = malloc(letters);
+		if (buff == NULL)
+		{
+			return (0);
+		}
+
+		ID = open(filename, O_RDONLY, 0600);
+		if (ID == -1)
+		{
+			free(buff);
+			return (0);
+		}
+		len = read(ID, buff, letters);
+		if (len == -1)
+		{
+			free(buff);
+			return (0);
+		}
+		WR = write(STDOUT_FILENO, buff, len);
+		if (WR == -1 || WR < len)
+		{
+			free(buff);
+			return (0);
+		}
+		close(ID);
+		free(buff);
+		return (WR);
 	}
-
-	let = read(file, text, letters);
-
-	w = write(STDOUT_FILENO, text, let);
-
-	close(file);
-
-	return (w);
+	return (0);
 }
