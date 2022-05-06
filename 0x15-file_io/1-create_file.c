482621dbbdab42405f624007bad9f6a344145706
Oliyad Kebede <81267448+OliyadKebede@users.noreply.github.com>
2022-05-06 10:58:48 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,37 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include "holberton.h"
 
 /**
- * create_file - function that creates a file.
- * @filename: variable pointer
- * @text_content: content file
- * Description: Create a function that creates a file.
- * Return: 1 on success, -1 on failure
+ * create_file - check the code for Holberton School students.
+ * @filename: name of my file
+ * @text_content: number of the letters that i used
+ * Return: Always 0.
  */
 
 int create_file(const char *filename, char *text_content)
 {
-	int i = 0, file;
+	int ID = 0, wr = 0, count = 0;
 
 	if (filename == NULL)
-		return (-1);
-
-	if (text_content == NULL)
-		text_content = "";
-
-
-	while (text_content[i] != '\0')
 	{
-		i++;
-	}
-
-	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-	if (file == -1)
 		return (-1);
-
-	write(file, text_content, i);
-
+	}
+	if (text_content != NULL)
+	{
+		for (count = 0; text_content[count] != '\0'; count++)
+		{}
+	}
+	ID = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	if (ID == -1)
+	{
+		return (-1);
+	}
+	wr = write(ID, text_content, count);
+	if (wr == -1)
+	{
+		return (-1);
+	}
+	close(ID);
 	return (1);
 }
