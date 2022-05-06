c1e7b377d53879788c3e70776e2589eec784dbb3
Oliyad Kebede <81267448+OliyadKebede@users.noreply.github.com>
2022-05-06 10:54:16 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,32 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "holberton.h"
 
 /**
- * create_file - create a file with read/write access for user
- * @filename: name of file to create
- * @text_content: string to write to file
+ * create_file - function that creates a file.
+ * @filename: variable pointer
+ * @text_content: content file
+ * Description: Create a function that creates a file.
  * Return: 1 on success, -1 on failure
  */
+
 int create_file(const char *filename, char *text_content)
 {
-int fd, rstatus, i;
+	int i = 0, file;
 
-if (filename == NULL)
-	return (-1);
-
-fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-
-if (fd == -1)
-	return (-1);
-
-if (text_content)
-{
-	for (i = 0; text_content[i] != '\0'; i++)
-		;
-	rstatus = write(fd, text_content, i);
-	if (rstatus == -1)
+	if (filename == NULL)
 		return (-1);
-}
 
-close(fd);
-return (1);
+	if (text_content == NULL)
+		text_content = "";
+
+
+	while (text_content[i] != '\0')
+	{
+		i++;
+	}
+
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (file == -1)
+		return (-1);
+
+	write(file, text_content, i);
+
+	return (1);
 }
