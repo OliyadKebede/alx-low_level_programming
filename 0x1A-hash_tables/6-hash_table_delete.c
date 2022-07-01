98e248707fd36ec33d01eecce4c5ba30a19c21d7
Oliyad Kebede <81267448+OliyadKebede@users.noreply.github.com>
2022-07-01 16:57:15 -0700

    Update 6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,43 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "hash_tables.h"
+
 /**
- * hash_table_delete - deletes a hash table
- * @ht: the hash table you want to delete
- */
+  * hash_table_delete - Deletes a hash table
+  * @ht: The hash table to frees
+  *
+  * Return: Nothing!
+  */
 void hash_table_delete(hash_table_t *ht)
 {
-unsigned long int i;
-hash_node_t *node;
+	unsigned long int i = 0;
+	hash_node_t *node = NULL, *temp = NULL;
 
-if (ht == NULL)
-	return;
-
-for (i = 0; i < ht->size; i++)
-{
-	node = ht->array[i];
-	free_hash_list(node);
-}
-free(ht->array);
-free(ht);
-}
-
-
-/**
- * free_hash_list - frees a hash_node_t list
- * @head: head of linked list
- */
-void free_hash_list(hash_node_t *head)
-{
-	hash_node_t *current;
-	hash_node_t *next;
-
-	current = head;
-
-	while (current != NULL)
+	if (ht && ht->size && ht->array)
 	{
-		next = current->next;
-		free(current->key);
-		free(current->value);
-		free(current);
-		current = next;
+		for (; i < ht->size; ++i)
+		{
+			node = ht->array[i];
+
+			if (node)
+			{
+				if (node->next)
+				{
+					node = node->next;
+					while (node)
+					{
+						temp = node;
+						node = node->next;
+						free(temp->key);
+						free(temp->value);
+						free(temp);
+					}
+				}
+
+				node = ht->array[i];
+				if (node->key && node->value)
+				{
+					free(node->key);
+					free(node->value);
+				}
+			}
+
+			free(node);
+		}
+
+		free(ht->array);
+		free(ht);
 	}
 }
