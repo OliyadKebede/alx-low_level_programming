040fcb55c4715006f6de46ba29f35ea1322b214a
Oliyad Kebede <81267448+OliyadKebede@users.noreply.github.com>
2022-10-04 18:51:59 -0700

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -29,5 +29,5 @@ int exponential_search(int *array, size_t size, int value);
 int mod_binary_search(int *array, int value, int begin, int end);
 int advanced_binary(int *array, size_t size, int value);
 int recursive_binary(int *array, int value, int begin, int end);
-listint_t *jump_list(listint_t *list, size_t size, int value)
+listint_t *jump_list(listint_t *list, size_t size, int value);
 #endif
